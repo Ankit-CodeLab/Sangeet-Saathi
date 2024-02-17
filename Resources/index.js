827a56b2c4b21a34234ca5d763997167f90712a3
Ankit Kushwaha <143326279+Ankit-CodeLab@users.noreply.github.com>
2024-02-17 18:14:09 +0530

    Add files via upload

---
+++ Resources/index.js
@@ -0,0 +1,54 @@
+let currentsong = new Audio();
+
+
+async function getsongs() {
+
+    let a = await fetch("http://127.0.0.1:3000/Music%20Player/Resources/Songs/")
+    let response = await a.text();
+    console.log(response)
+
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+
+    let songs = []
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/Songs/")[1])
+        }
+    }
+
+    return (songs)
+
+}
+
+const playmusic = (track) => {
+
+    currentsong.src = "Resources/Songs/" + track
+    currentsong.play();
+}
+
+async function main() {
+
+    let songs = await getsongs();
+    console.log(songs)
+
+    let songUl = document.querySelector(".Songs-list").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUl.innerHTML += `<li> ${song.replaceAll("%20"," ")} </li>`;
+    }
+
+    Array.from(document.querySelector(".Songs-list").getElementsByTagName("li")).forEach(e=> {
+        e.addEventListener("click" , element => {
+            console.log(e.innerHTML)
+            playmusic(e.innerHTML.trim())
+        })
+    });
+ 
+
+}
+
+main()
